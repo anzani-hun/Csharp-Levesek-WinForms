e4f6e910238d21a522c854283a66ecc0e13ba67f
anzani-hun <jerikostreet@gmail.com>
2024-04-26 17:29:36 +0200

    lista megjelenítéssel

--- Csharp-Levesek-WinForms/Form1.Designer.cs
+++ Csharp-Levesek-WinForms/Form1.Designer.cs
@@ -53,7 +53,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(655, 320);
+            this.pictureBox1.Location = new System.Drawing.Point(655, 382);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(133, 118);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -64,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(99, 118);
+            this.label1.Location = new System.Drawing.Point(99, 144);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 20);
             this.label1.TabIndex = 1;
@@ -74,7 +74,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(99, 153);
+            this.label2.Location = new System.Drawing.Point(99, 179);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 20);
             this.label2.TabIndex = 2;
@@ -84,7 +84,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(99, 191);
+            this.label3.Location = new System.Drawing.Point(99, 217);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 20);
             this.label3.TabIndex = 3;
@@ -94,7 +94,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(99, 230);
+            this.label4.Location = new System.Drawing.Point(99, 256);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 20);
             this.label4.TabIndex = 4;
@@ -104,7 +104,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(99, 264);
+            this.label5.Location = new System.Drawing.Point(99, 290);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(101, 20);
             this.label5.TabIndex = 5;
@@ -114,7 +114,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(99, 296);
+            this.label6.Location = new System.Drawing.Point(99, 322);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(61, 20);
             this.label6.TabIndex = 6;
@@ -124,7 +124,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(99, 330);
+            this.label7.Location = new System.Drawing.Point(99, 356);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(52, 20);
             this.label7.TabIndex = 7;
@@ -132,7 +132,7 @@
             // 
             // text_Megnevezes
             // 
-            this.text_Megnevezes.Location = new System.Drawing.Point(219, 118);
+            this.text_Megnevezes.Location = new System.Drawing.Point(219, 144);
             this.text_Megnevezes.Name = "text_Megnevezes";
             this.text_Megnevezes.Size = new System.Drawing.Size(200, 20);
             this.text_Megnevezes.TabIndex = 8;
@@ -141,52 +141,52 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label8.Location = new System.Drawing.Point(140, 29);
+            this.label8.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label8.Location = new System.Drawing.Point(129, 34);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(560, 39);
+            this.label8.Size = new System.Drawing.Size(532, 39);
             this.label8.TabIndex = 9;
-            this.label8.Text = "Levesek felvétele az adatbázisba";
+            this.label8.Text = "Leves felvétele az adatbázisba!";
             this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // text_Kaloria
             // 
-            this.text_Kaloria.Location = new System.Drawing.Point(219, 153);
+            this.text_Kaloria.Location = new System.Drawing.Point(219, 179);
             this.text_Kaloria.Name = "text_Kaloria";
             this.text_Kaloria.Size = new System.Drawing.Size(200, 20);
             this.text_Kaloria.TabIndex = 10;
             // 
             // text_Feherje
             // 
-            this.text_Feherje.Location = new System.Drawing.Point(219, 190);
+            this.text_Feherje.Location = new System.Drawing.Point(219, 216);
             this.text_Feherje.Name = "text_Feherje";
             this.text_Feherje.Size = new System.Drawing.Size(200, 20);
             this.text_Feherje.TabIndex = 11;
             // 
             // text_Zsir
             // 
-            this.text_Zsir.Location = new System.Drawing.Point(219, 230);
+            this.text_Zsir.Location = new System.Drawing.Point(219, 256);
             this.text_Zsir.Name = "text_Zsir";
             this.text_Zsir.Size = new System.Drawing.Size(200, 20);
             this.text_Zsir.TabIndex = 12;
             // 
             // text_Szenhidrat
             // 
-            this.text_Szenhidrat.Location = new System.Drawing.Point(219, 263);
+            this.text_Szenhidrat.Location = new System.Drawing.Point(219, 289);
             this.text_Szenhidrat.Name = "text_Szenhidrat";
             this.text_Szenhidrat.Size = new System.Drawing.Size(200, 20);
             this.text_Szenhidrat.TabIndex = 13;
             // 
             // text_Hamu
             // 
-            this.text_Hamu.Location = new System.Drawing.Point(219, 296);
+            this.text_Hamu.Location = new System.Drawing.Point(219, 322);
             this.text_Hamu.Name = "text_Hamu";
             this.text_Hamu.Size = new System.Drawing.Size(200, 20);
             this.text_Hamu.TabIndex = 14;
             // 
             // text_Rost
             // 
-            this.text_Rost.Location = new System.Drawing.Point(219, 330);
+            this.text_Rost.Location = new System.Drawing.Point(219, 356);
             this.text_Rost.Name = "text_Rost";
             this.text_Rost.Size = new System.Drawing.Size(200, 20);
             this.text_Rost.TabIndex = 15;
@@ -194,9 +194,9 @@
             // button_LevesHozzaad
             // 
             this.button_LevesHozzaad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button_LevesHozzaad.Location = new System.Drawing.Point(166, 376);
+            this.button_LevesHozzaad.Location = new System.Drawing.Point(103, 419);
             this.button_LevesHozzaad.Name = "button_LevesHozzaad";
-            this.button_LevesHozzaad.Size = new System.Drawing.Size(253, 49);
+            this.button_LevesHozzaad.Size = new System.Drawing.Size(316, 49);
             this.button_LevesHozzaad.TabIndex = 16;
             this.button_LevesHozzaad.Text = "Új leves hozzáadása";
             this.button_LevesHozzaad.UseVisualStyleBackColor = true;
@@ -207,15 +207,15 @@
             this.levesLista.FormattingEnabled = true;
             this.levesLista.Location = new System.Drawing.Point(452, 117);
             this.levesLista.Name = "levesLista";
-            this.levesLista.Size = new System.Drawing.Size(150, 303);
+            this.levesLista.Size = new System.Drawing.Size(150, 368);
             this.levesLista.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 512);
             this.Controls.Add(this.levesLista);
             this.Controls.Add(this.button_LevesHozzaad);
             this.Controls.Add(this.text_Rost);
@@ -235,7 +235,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Levesek felvétele";
+            this.Text = "Levesek felvétele ^.^";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
